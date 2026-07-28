f56130184fd2c099216fd3b619a4e3400519b050
apprat <apprat@qq.com>
2026-07-28 19:54:46 +0800

    update v1

--- dev/build.js
+++ dev/build.js
@@ -8,8 +8,10 @@ for (const style of fs.readdirSync(getPath('icons'))) {
   const styleDir = fs.readdirSync(getPath('icons', style))
   const newDir = getPath('components', style)
   if (!fs.existsSync(newDir)) fs.mkdirSync(newDir)
-  let styleAll = ''
-  let styleFillAll = ''
+  const all = {
+    def: `import { options as o } from '../../main.js'\n`,
+    fill: `import { options as o } from '../../main.js'\n`
+  }
   for (const name of styleDir) {
     const text = fs.readFileSync(getPath('icons', style, name)).toString()
     const newName = name.slice(0, -4)
@@ -18,10 +20,9 @@ for (const style of fs.readdirSync(getPath('icons'))) {
     const match = text.match(/<path\b[^>]*\bd\s*=\s*(["'])(.*?)\1/i)
     const d = match?.[2]
     fs.writeFileSync(getPath('components', style, `${newName}.js`), `import { options } from '../../main.js'\noptions.icons['${prefix}${newName}']='${d}'`)
-    styleAll += `import './${style}/${newName}.js'\n`
-    isFill ? styleFillAll += `import './${style}/${newName}.js'\n` : styleAll += `import './${style}/${newName}.js'\n`
+    all[isFill ? 'fill' : 'def'] += `o.icons['${prefix}${newName}']='${d}'\n`
     console.log(`${style}/${name}`)
   }
-  fs.writeFileSync(getPath('components', `${style}.js`), styleAll)
-  fs.writeFileSync(getPath('components', `${style}-fill.js`), styleFillAll)
+  fs.writeFileSync(getPath('components', `${style}.js`), all.def)
+  fs.writeFileSync(getPath('components', `${style}-fill.js`), all.fill)
 }
